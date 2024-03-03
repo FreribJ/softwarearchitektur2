ab4ce2cd09320c89d434eeb99539234a824be257
finn <finn-matthis.veerkamp@hsw-stud.de>
2024-03-03 11:33:56 +0100

    Microapp edit metadata entfernt

--- microapps/UI-Zoo/src/app/app.routes.ts
+++ microapps/UI-Zoo/src/app/app.routes.ts
@@ -5,7 +5,6 @@ import {AppComponent} from "./app.component";
 import {HomepageComponent} from "./homepage/homepage.component";
 import {TourOverviewComponent} from "./tour-component/tour-overview/tour-overview.component";
 import {AttractionOverviewComponent} from "./attraction-component/attraction-overview/attraction-overview.component";
-import {EditMetadataComponent} from "./homepage/edit-metadata/edit-metadata.component";
 
 export const routes: Routes = [
   {path: 'tours', component: ToursComponent},
@@ -13,5 +12,4 @@ export const routes: Routes = [
   {path: 'attractions', component: AttractionsComponent},
   {path: 'attraction-overview/:id', component: AttractionOverviewComponent},
   {path: 'tour-overview/:id', component: TourOverviewComponent},
-  {path: 'editMetadata', component: EditMetadataComponent},
 ];
