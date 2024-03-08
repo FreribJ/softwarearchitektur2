3475e9cb3c043975ee0ef8f3ed99c01cf9e13e01
Frerib_J <frerib.j@gmail.com>
2024-03-08 20:31:28 +0100

    refactor

--- microservices/bird-park-management/src/main/java/com/hsw/birdparkmanagement/api/ui/WebConfiguration.java
+++ microservices/bird-park-management/src/main/java/com/hsw/birdparkmanagement/api/ui/WebConfiguration.java
@@ -3,8 +3,10 @@ package com.hsw.birdparkmanagement.api.ui;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+//@EnableSwagger2
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
