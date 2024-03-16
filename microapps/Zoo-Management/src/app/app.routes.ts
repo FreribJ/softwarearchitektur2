60cd7183d787d84e1e998957342db4dc38de6509
finn <finn-matthis.veerkamp@hsw-stud.de>
2024-03-16 01:27:52 +0100

    Routen gemacht

--- microapps/Zoo-Management/src/app/app.routes.ts
+++ microapps/Zoo-Management/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {EditTourComponent} from "./edit-tour/edit-tour.component";
+import {AppComponent} from "./app.component";
+import {EditAttractionComponent} from "./edit-attraction/edit-attraction.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: 'tourEdit', component: EditTourComponent},
+  {path: 'attractionEdit', component: EditAttractionComponent},
+  {path: '', component: AppComponent}];
